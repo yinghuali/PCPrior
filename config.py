0442a7c9bb926388b001ec625ffb175e3366cdac
yinghuali <yinghua.li@uni.lu>
2023-06-12 01:26:07 +0200

    update

--- config.py
+++ config.py
@@ -3,10 +3,3 @@ dic_modelnet40 = {'airplane': 0, 'bathtub': 1, 'bed': 2, 'bench': 3, 'bookshelf'
                   'keyboard': 18, 'lamp': 19, 'laptop': 20, 'mantel': 21, 'monitor': 22, 'night_stand': 23, 'person': 24, 'piano': 25,
                   'plant': 26, 'radio': 27, 'range_hood': 28, 'sink': 29, 'sofa': 30, 'stairs': 31, 'stool': 32, 'table': 33, 'tent': 34,
                   'toilet': 35, 'tv_stand': 36, 'vase': 37, 'wardrobe': 38, 'xbox': 39}
-
-
-dic_shapenet = {'02691156': 0, '02773838': 1, '02954340': 2, '02958343': 3, '03001627': 4, '03261776': 5, '03467517': 6, '03624134': 7,
- '03636649': 8, '03642806': 9, '03790512': 10, '03797390': 11, '03948459': 12, '04099429': 13, '04225987': 14, '04379243': 15}
-
-
-
