5f5ec395d26fc472b481a86d14bdb632a8e0a3eb
yinghuali <yinghua.li@uni.lu>
2023-07-24 14:33:22 +0200

    update

--- pcprior.py
+++ pcprior.py
@@ -32,8 +32,6 @@ path_train_point_mutants_feature = args.path_train_point_mutants_feature
 path_test_point_mutants_feature = args.path_test_point_mutants_feature
 path_save_res = args.path_save_res
 
-# python pcprior.py --path_x '/raid/yinghua/PCPrior/pkl_data/modelnet40/X.pkl' --path_y '/raid/yinghua/PCPrior/pkl_data/modelnet40/y.pkl' --path_target_model './target_models/modelnet40_pointnet_2.pt' --path_target_model_train_pre '/raid/yinghua/PCPrior/pkl_data/modelnet40_pre/modelnet40_pre_pointnet_2_train_pre.pkl' --path_target_model_test_pre '/raid/yinghua/PCPrior/pkl_data/modelnet40_pre/modelnet40_pre_pointnet_2_test_pre.pkl' --path_train_point_mutants_feature '/raid/yinghua/PCPrior/pkl_data/modelnet40/pointnet_2_train_point_mutants_feature_vec.pkl' --path_test_point_mutants_feature '/raid/yinghua/PCPrior/pkl_data/modelnet40/pointnet_2_test_point_mutants_feature_vec.pkl' --path_save_res './result/modelnet40_pointnet_2.json'
-
 
 def main():
     model = torch.load(path_target_model)
